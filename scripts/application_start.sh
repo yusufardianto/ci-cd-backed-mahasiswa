f715d86caf1c0a19f98a1487b85c730c199c15b2
yusufardianto <muhammadyusufardianto@gmail.com>
2023-05-08 22:28:09 +0700

    upload

---
+++ scripts/application_start.sh
@@ -0,0 +1,16 @@
+# menjalankan perintah sebagai admin(sementara) untuk mengelola directory
+sudo chmod -R 777 /home/ec2-user/mahasiswa
+
+# pindah ke directory
+cd /home/ec2-user/node-aws
+
+# add npm and node to path
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# install node modul
+npm install
+
+# start node app ind the background
+node app.js > app.out.log 2> app.err.log < /dev/null &
