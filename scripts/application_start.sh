7d4c7b19da2b75d8f22ef13ecd2a8462f7c72b18
Muhammad Yusuf Ardianto <56336225+yusufardianto@users.noreply.github.com>
2023-05-26 18:01:16 +0700

    update script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -13,4 +13,4 @@ export NVM_DIR="$HOME/.nvm"
 npm install
 
 # start node app ind the background
-node app.js > app.out.log 2> app.err.log < /dev/null &
+node index.js > app.out.log 2> app.err.log < /dev/null &
